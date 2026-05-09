09d8f9284063652970ca31e03e42917d3081ba6a
xRoyse <dimkafernandez@gmail.com>
2026-05-09 11:54:43 +0300

    Создан проект

---
+++ VorteX - Calculator for IT companies/VorteX - Calculator for IT companies/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VorteX___Calculator_for_IT_companies
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
